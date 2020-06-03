ba2fd08bb8f3bb90c63432293d51ee5a0907f4e8
crashdemons <crashenator@gmail.com>
2020-06-02 23:37:57 -0500

    document config util class

--- src/main/java/net/evmodder/scoreboarduuid/ConfigUtil.java
+++ src/main/java/net/evmodder/scoreboarduuid/ConfigUtil.java
@@ -7,21 +7,33 @@ package net.evmodder.scoreboarduuid;
 
 import com.github.crashdemons.scoreboarduuid.ScoreboardUpdateBehavior;
 import java.util.HashMap;
+import java.util.Map;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
- *
+ * Utility class for retrieving ScoreboardUUID config options
  * @author EvModder
  */
 public class ConfigUtil {
     private ConfigUtil(){}//static utility class only
     
+    /**
+     * Gets the value of the reset-old-scores setting.
+     * Note: defaults to 'true'
+     * @param plugin the plugin to retrieve the configuration for
+     * @return the configured value, or True if not present.
+     */
     public static boolean getResetOldScores(JavaPlugin plugin){
         return plugin.getConfig().getBoolean("reset-old-scores", true);
     }
     
-    public static HashMap<String, ScoreboardUpdateBehavior> getScoresToUpdate(JavaPlugin plugin) throws IllegalStateException{
+    /**
+     * Gets the configured list of scores to transfer on name changes with their associated update behavior.
+     * @param plugin the plugin to retrieve the configuration for
+     * @return a Map of scores configured to be transferred and their associated behaviors. 
+     */
+    public static HashMap<String, ScoreboardUpdateBehavior> getScoresToUpdate(JavaPlugin plugin){
         if (!plugin.getConfig().isConfigurationSection("uuid-based-scores")) {
             return null;
             //getLogger().warning("No uuid-based scores found in config! Disabling plugin");
