6e57aa8637c3f3a80560fc8590f5a063770ee784
crashdemons <crashenator@gmail.com>
2020-06-02 23:19:07 -0500

    document tag util

--- src/main/java/net/evmodder/scoreboarduuid/TagUtil.java
+++ src/main/java/net/evmodder/scoreboarduuid/TagUtil.java
@@ -8,11 +8,17 @@ package net.evmodder.scoreboarduuid;
 import org.bukkit.entity.Player;
 
 /**
- *
+ * Utility class for handling scoreboard tags tracking player username history
  * @author EvModder
  */
 public class TagUtil {
     private TagUtil(){}//static utility class only
+    
+    /**
+     * Retrieves the previously recorded username for the player provided
+     * @param player the player to check
+     * @return the player's previous name, or their current name if none is found.
+     */
     public static String getPreviousName(Player player) {
         for (String tag : player.getScoreboardTags()) {
             if (tag.startsWith("prev_name_")) {
@@ -22,6 +28,13 @@ public class TagUtil {
         return player.getName();
     }
     
+    /**
+     * Updates tracking information for a players 'previous' name.
+     * This should be used when you are finished updating all information for the user, or an old name doesn't exist.
+     * @param player the player to update
+     * @param prevName the name the user had before
+     * @param currName the current name of the user to record before the next change.
+     */
     public static void updatePreviousName(Player player, String prevName, String currName){
         player.removeScoreboardTag("prev_name_" + prevName);
         player.addScoreboardTag("prev_name_" + currName);
